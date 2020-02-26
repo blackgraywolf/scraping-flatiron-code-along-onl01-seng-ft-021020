eff889ee5846aa4e20cfc4cf5d647fd3fff5aae6
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-26 18:31:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,10 +7,11 @@ require_relative './course.rb'
 class Scraper
  
   def get_page
-      # more code coming soon!
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
   end
  
 end
-
-
+ 
+Scraper.new.get_page
